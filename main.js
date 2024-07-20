320b1017e0f4b737d4d04cd31d616f7786716177
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2024-07-20 16:07:43 +0500

    4 oy 4 dars

---
+++ main.js
@@ -0,0 +1,16 @@
+// const random = (Math.round(Math.random() * 100))
+// alert = (`Random: ${random}`)
+
+// if (random % 2 == 0) {
+//     alert (`juft ${random}`)
+// } else {
+//     alert (`toq ${random}`)
+// }
+
+const random = (Math.round(Math.random() * 100))
+alert(`Рандомное число: ${random}`)
+if (random % 2 == 0 ) {
+    alert(`${random} чотное `)
+} else {
+    alert(`${random} нечотное `)
+}
